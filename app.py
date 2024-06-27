454a6f6aeb071f2360a207d1748beb85eaf7657c
swarup2137 <swarupraut0@gmail.com>
2024-06-27 20:11:53 +0530

    added

---
+++ app.py
@@ -0,0 +1,19 @@
+#         JAI SHREE RAM
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+    # return "Hello, World!"
+
+
+@app.route("/product")
+def product():
+    return "dgbfhdh"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
